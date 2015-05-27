fc9bc7af4a6dc4304e8bc9837a3544b963a333c8
Damien McParland <damien.mcparland@ucd.ie>
2015-05-27 00:00:00 +0000

    version 1.1

--- R/perc.cutoffs.R
+++ R/perc.cutoffs.R
@@ -1,8 +1,8 @@
 perc.cutoffs <-
 function(CnsIndx, OrdIndx, Y, N){
-  perc.cut <- list()
-  for(j in (CnsIndx+1):OrdIndx){
-    perc.cut[[j]] <- qnorm(c(0, cumsum(table(Y[, j])/N)))
+    perc.cut <- list()
+    for(j in (CnsIndx+1):OrdIndx){
+      perc.cut[[j]] <- qnorm(c(0, cumsum(table(Y[, j])/N)))
+    }
+    perc.cut
   }
-  perc.cut
-}
